8c410fb6792d2fba7be9ae3a40f6092820ab2213
Karim <kwkkna3@gnail.com>
2025-10-30 08:12:43 +0300

    Fixing a color issue in cryptoolUI.py

--- cryptoolUI.py
+++ cryptoolUI.py
@@ -48,7 +48,7 @@ class CryptoUI():
                     else:
                         self.crypto.decrypt(encryption_key_file, operation_file)
 
-                    print(f"{BOLD}{BG_BLUE}==> Encrypting {operation_file} ...")
+                    print(f"{BOLD}{BG_BLUE}==> {operation_name}ing {operation_file} ...")
                     print(f"{BOLD}{FG_GREEN}==> Successfully {operation_name}ed: {operation_file}")
                     self.cryptoutils.pause()
                     break
